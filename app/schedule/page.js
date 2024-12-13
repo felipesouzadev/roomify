a12713dac7d123d25ddf0cc0495b3e15052f4f88
felipesouzadev <felipesouza.dev@gmail.com>
2024-12-13 14:27:22 -0300

    Add schedule filter with teacher or room

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,26 +1,68 @@
 'use client'
 import { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import { Card, Modal, ModalHeader, ModalBody, ModalContent, ModalFooter, useDisclosure, Button} from "@nextui-org/react";
+import { Card, Modal, ModalHeader, ModalBody, ModalContent, ModalFooter, useDisclosure, Button, Select, SelectItem, Switch} from "@nextui-org/react";
 import axios from "axios"
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import PageWrapper from '../../components/PageWrapper';
 import GetCurrentWeek from "../utils/GetCurrentWeek";
+import PageActions from "../../components/PageActions";
 
 export default function Schedule(){
     const localizer = momentLocalizer(moment);
     const { sunday, friday } = GetCurrentWeek(new Date());
     const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [filterType, setFilterType] = useState("");
+    const [teachers, setTeachers] = useState([]);
+    const [selectedTeachers, setSelectedTeachers] = useState([]);
+    const [selectedRooms, setSelectedRooms] = useState([]);
+    const [rooms, setRooms] = useState([]);
+    const [fileredRooms, setFilteredRooms] = useState([]);
+    const [filterEnabled, setFilteredEnabled] = useState(false);
     const [startDate, setStartDate] = useState(sunday);
     const [endDate, setEndDate] = useState(friday);
     const [filteredDates, setFilteredDates] = useState([]);
     const currentDate = new Date();
     const [selectedEvent, setSelectedEvent] = useState({title: ''});
     const {isOpen: isDeleteOpen, onOpen: onDeleteOpen, onOpenChange: onOpenDeleteChange} = useDisclosure();
+    const idRegex = /room(\d+)-teacher(\d+)-weekday(\d+)-shift(MORNING|AFTERNOON|NIGHT)/;
+
+      useEffect(() => {
+        axios.get('/api/teachers')
+          .then((response) => {
+            setTeachers(response.data)
+          });
+      }, []);
+
+      useEffect(() => {
+        axios.get('/api/rooms')
+          .then((response) => {
+            setRooms(response.data);
+          });
+      }, []);
+
+      useEffect(() => {
+        if(filterEnabled) {
+          if(filterType === "teacher" && selectedTeachers.length > 0){
+            setFilteredEvents(events.filter((event) => {
+              const match = event.id.match(idRegex);
+              const teacherId = match[2];
+              return selectedTeachers.includes(parseInt(teacherId))
+            }));
+          } else if(filterType === "room" && selectedRooms.length > 0) {
+            setFilteredEvents(events.filter((event) => {
+              const match = event.id.match(idRegex);
+              const roomId = match[1];
+              return selectedRooms.includes(parseInt(roomId))
+  
+            }));
+          }
+        }
+      }, [filterType, selectedTeachers, selectedRooms, filterEnabled]);
 
     const handleOpenDeleteModal = (event) => {
-      console.log(event)
       setSelectedEvent(event);
       onDeleteOpen();
     }
@@ -85,11 +127,66 @@ export default function Schedule(){
   
     return (
       <PageWrapper>
-        <h1 className="text-3xl font-bold text-center mb-4">Schedule</h1>
+        <div className="flex">
+        <h1 color="primary" className="text-3xl font-bold text-center mb-4">Schedule</h1>
+          <PageActions>
+            <div className="flex justify-end w-full items-end gap-3">
+            <Select
+            color="primary"
+            id="filterType"
+            value={filterType}
+            label="Filter"
+            labelPlacement="outside"
+            className="rounded max-w-28"
+            size="sm"
+            onChange={(e) => setFilterType(e.target.value)}>
+              <SelectItem key="teacher">Teacher</SelectItem>
+              <SelectItem key="room">Room</SelectItem>
+            </Select>
+            {filterType === "teacher" && (
+              <Select
+                color="primary"
+                id="teacher"
+                value={filterType}
+                labelPlacement="outside"
+                label="Select Teacher"
+                selectionMode="multiple"
+                className="rounded max-w-28"
+                size="sm"
+                onChange={(e) => setSelectedTeachers(e.target.value.split(",").map(Number))}>
+                {teachers.map((teacher) => (
+                  <SelectItem key={teacher.id} value={teacher.id}>
+                    {teacher.name}
+                  </SelectItem>
+                ))}
+              </Select>
+            )}
+            {filterType === "room" && (
+              <Select
+                color="primary"
+                id="room"
+                value={filterType}
+                label="Select Room"
+                labelPlacement="outside"
+                selectionMode="multiple"
+                className="rounded max-w-28"
+                size="sm"
+                onChange={(e) => setSelectedRooms(e.target.value.split(",").map(Number))}>
+                {rooms.map((room) => (
+                  <SelectItem key={room.id} value={room.id}>
+                    {room.name}
+                  </SelectItem>
+                ))}
+              </Select>
+            )}
+            <Switch  color="primary" size="sm" onChange={() => setFilteredEnabled(!filterEnabled)} />
+            </div>
+          </PageActions>
+        </div>
         <Card className="bg-background text-foreground">
             <Calendar
               localizer={localizer}
-              events={events}
+              events={filterEnabled ? filteredEvents :  events}
               defaultDate={currentDate}
               startAccessor="start"
               endAccessor="end"
